d9de1485a3abe1a9277e0b9ba5ade9efc11ae6fa
liorean <liorean@gmail.com>
2016-03-21 00:29:52 +0100

    Fixing exception handling

--- win64/examples/000 The basics/helloworld.asm
+++ win64/examples/000 The basics/helloworld.asm
@@ -20,7 +20,21 @@ section .data use64
     msglen:             equ     $-msg
     zero:               equ     0x0
     STD_OUTPUT_HANDLE:  equ     -11
-
+    UNW_VERSION:        equ     0x1
+    UNW_FLAG_NHANDLER:  equ     0x0
+    UNW_FLAG_EHANDLER:  equ     0x1
+    UNW_FLAG_UHANDLER:  equ     0x2
+    UNW_FLAG_CHAININFO: equ     0x4
+    UWOP_PUSH_NONVOL:   equ     0x0
+    UWOP_ALLOC_LARGE:   equ     0x1
+    UWOP_ALLOC_SMALL:   equ     0x2
+    UWOP_SET_FPREG:     equ     0x3
+    UWOP_SAVE_NONVOL:   equ     0x4
+    UWOP_SAVE_NONVOL_FAR:equ    0x5
+    UWOP_SAVE_XMM128:   equ     0x8
+    UWOP_SAVE_XMM128_FAR:equ    0x9
+    UWOP_PUSH_MACHFRAME:equ     0xa
+  
 section .bss use64
     hStdOutput  resq    0x1
     hNum        resq    0x1
@@ -66,10 +80,17 @@ section .pdata  rdata align=4 use64
 section .xdata  rdata align=8 use64
     xmain:
     .versionandflags:
-            db      0x1 << 0x5 + 0x0 ; Version = 1, UNW_FLAG_NHANDLER flag
+            db      UNW_VERSION + (UNW_FLAG_NHANDLER << 0x3) ; Version = 1
+    ; Version is lowest 3 bits, Handler flaga are highest 5 bits
     .size:  db      main.body-main.prolog ; size of prolog that is
     .count: db      0x1 ; Only one unwind code
-    .frame: db      0x0 ; Zero if no frame pointer taken
+    .frame: db      0x0 + (0x0 << 0x4) ; Zero if no frame pointer taken
+    ; Frame register is low 4 bits, Frame register offset is RSP + 16 * offset
+    ; at time of establishing
     .codes: db      main.body-main.prolog ; offset of next instruction
-            db      0x24 ; UWOP_ALLOC_SMALL with 4*8+8 bytes
+            db      UWOP_ALLOC_SMALL + (0x4 << 0x4) ; UWOP_INFO: 4*8+8 bytes
+    ; Low 4 bytes UWOP, High 4 bytes op info.
+    ; Some ops use one or two 16 bit slots more for addressing here
             db      0x0,0x0 ; Unused record to bring the number to be even
+    .handl: ; 32 bit image relative address to entry of exception handler
+    .einfo: ; implementation defined structure exception info
