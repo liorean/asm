cfc61d75c766ffd377a57c2410b6de26c4ddc3ce
liorean <liorean@gmail.com>
2016-03-23 05:54:11 +0100

    Tell user when assembling/linking/running and error

--- win64/examples/000 The basics/comprun.bat
+++ win64/examples/000 The basics/comprun.bat
@@ -5,25 +5,38 @@ set lst="%1.lst"
 set exe="%1.exe"
 if exist %obj% del %obj%
 
+echo ----
+echo Assembling Program
+echo ----
 nasm -o %obj% -f win64 -l %lst% %asm%
 if errorlevel 1 goto asmerr
 
+echo ----
+echo Linking Program
+echo ----
 golink /console /ni /entry main %obj% kernel32.dll user32.dll gdi32.dll msvcrt.dll comctl32.dll comdlg32.dll oleaut32.dll hhctrl.ocx winspool.drv shell32.dll
 if errorlevel 1 goto linkerr
 
 if exist %obj% del %obj%
 goto runit
 
+:runerr
+echo ----
+echo Error while Running Program!
+echo ----
+pause
+goto fin
+
 :linkerr
 echo ----
-echo Error while Linking
+echo Error while Linking!
 echo ----
 pause
 goto fin
 
 :asmerr
 echo ----
-echo Error while Assembling
+echo Error while Assembling!
 echo ----
 pause
 goto fin
@@ -33,6 +46,7 @@ echo ----
 echo Running Program
 echo ----
 %exe%
+if errorlevel 1 goto runerr
 
 :fin
 echo ----
